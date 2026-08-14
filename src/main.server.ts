bce51e228a96b2d57cc4a8f7c8df8732f6400dde
Ivan <mi55737@zut.edu.pl>
2026-08-14 20:24:45 +0200

    fix: resolve module import error in main.ts

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,9 @@
 import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
+import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { AppComponent } from './app/app';
 
 const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(App, config, context);
+    bootstrapApplication(AppComponent, config, context);
 
 export default bootstrap;

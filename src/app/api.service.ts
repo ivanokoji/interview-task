8df82bdd599614cc20ce59439afee3c1316cc515
Ivan <mi55737@zut.edu.pl>
2026-08-16 13:38:37 +0200

    finished testing the web-application

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   private apiUrl = 'https://api.api-ninjas.com/v1/imagetotext';
-  private apiKey = 'YOUR_API_KEY'; // Make sure your key is here
+  private apiKey = 'YOUR_API_KEY'; 
 
   constructor(private http: HttpClient) {}
 
@@ -17,8 +17,7 @@ export class ApiService {
 
     const headers = new HttpHeaders({
       'x-api-key': this.apiKey
-      // DO NOT manually set 'Content-Type': 'multipart/form-data'. 
-      // Angular and the browser must set it automatically with the boundary token!
+      
     });
 
     return this.http.post<any>(this.apiUrl, formData, { headers });
